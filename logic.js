9307936bfab877e8ee72a66020bf2082757f5284
alexlawson274 <65232122+alexlawson274@users.noreply.github.com>
2020-10-05 22:13:46 +1100

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,97 +1,74 @@
-// Create a map object
-var myMap = L.map("map", {
-  center: [15.5994, -28.6731],
-  zoom: 3
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
 });
 
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
+function createFeatures(earthquakeData) {
 
-// Country data
-var countries = [
-  {
-    name: "Manado, Indonesia",
-    location: [1.4748, 124.8421],
-    mag: 4.8
-  },
-  {
-    name: "New Caledonia",
-    location: [-21.8618 ,169.744 ],
-    mag: 4.5
-  },
-  {
-    name: "West Chile",
-    location:  [ -40.9358, -91.2401],
-    mag: 4.5
-  },
-  {
-    name: "Wainpagpu, Indonesia",
-    location: [ -9.667, 119.344 ],
-    mag: 4.9
-  },
-  {
-    name: "Oarai, Japan",
-    location: [36.3021, 140.8268 ],
-    mag: 4.9
-  },
-  {
-    name: "Off the coast of Portugal",
-    location: [37.5599, -31.8138 ],
-    mag: 5
-  },
-  {
-    name: "Tonga",
-    location: [-19.4391, -174.0583 ],
-    mag: 4.9
-  },
-  {
-    name: "Colombia ",
-    location: [4.1988, -71.386 ],
-    mag: 4.5
-  },
-  {
-    name: "Peru",
-    location: [-5.9836, -76.7288 ],
-    mag: 4.7
-  },
-  {
-    name: "Trinidad And Tobago",
-    location: [10.9775, -61.5217 ],
-    mag: 4.8
-  }
-];
-
-
-// Loop through the cities array and create one marker for each city object
-for (var i = 0; i < countries.length; i++) {
-
-  // Conditionals for countries mag
-  var color = "";
-  if (countries[i].mag > 5) {
-    color = "yellow";
-  }
-  else if (countries[i].mag > 4.9) {
-    color = "blue";
-  }
-  else if (countries[i].mag > 4.7) {
-    color = "green";
-  }
-  else {
-    color = "red";
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
 
-  // Add circles to map
-  L.circle(countries[i].location, {
-    fillOpacity: 0.75,
-    color: "white",
-    fillColor: color,
-    // Adjust radius
-    radius: countries[i].mag * 100000
-  }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>mag: " + countries[i].mag + "</h3>").addTo(myMap);
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Define streetmap and darkmap layers
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
 }
